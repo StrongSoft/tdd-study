710588f030b6d4c9762c5a384fd65a7931f222dc
regur007 <regur007@gmail.com>
2022-08-27 23:16:45 +0900

    9장end

--- src/main/java/com/regur/tddstudy/one/Dollar.java
+++ src/main/java/com/regur/tddstudy/one/Dollar.java
@@ -2,13 +2,13 @@ package com.regur.tddstudy.one;
 
 public class Dollar extends Money {
 
-  public Dollar(int amount) {
-    this.amount = amount;
+  public Dollar(int amount, String currency) {
+    super(amount, currency);
   }
 
   @Override
   Money times(int multiplier) {
-    return new Dollar(amount * multiplier);
+    return Money.dollar(amount * multiplier);
   }
 
 }
--- src/main/java/com/regur/tddstudy/one/Money.java
+++ src/main/java/com/regur/tddstudy/one/Money.java
@@ -2,21 +2,31 @@ package com.regur.tddstudy.one;
 
 public abstract class Money {
 
-  abstract Money times(int amount);
-
+  protected String currency;
   protected int amount;
 
+  public Money(int amount, String currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  abstract Money times(int amount);
+
   static Money dollar(int amount) {
-    return new Dollar(amount);
+    return new Dollar(amount, "USD");
   }
 
   static Money franc(int amount) {
-    return new Franc(amount);
+    return new Franc(amount, "CHF");
   }
 
   @Override
   public boolean equals(Object object) {
     Money money = (Money) object;
     return amount == money.amount && getClass().equals(money.getClass());
+  }
+
+  String currency() {
+    return currency;
   }
 }
